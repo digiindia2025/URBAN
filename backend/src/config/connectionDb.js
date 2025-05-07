84382a1bdfbdf56c7f32fface2e79e6ebeabcfde
digiindia2025 <amantiwariat2005@gmail.com>
2025-05-07 10:54:53 +0530

    g

---
+++ backend/src/config/connectionDb.js
@@ -0,0 +1,25 @@
+//server host name
+const HOST = "localhost";
+
+//remote database name
+const DATABASE = "urbanclone";
+
+//database credentials;
+const USERNAME = "urbanclone";
+const PASSWORD = "urbanclone";
+
+//default app port
+const PORT = process.env.PORT || 3333;
+
+//default connection url
+const DEFAULT_CONNECTION_STRING = process.env.MONGO_URL || `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.r35kx.mongodb.net/${DATABASE}?retryWrites=true&w=majority`;
+
+// mongoose options for connection
+const MONGOOSE_OPTIONS = {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+}
+
+module.exports = {HOST, PORT, DATABASE, USERNAME, PASSWORD, DEFAULT_CONNECTION_STRING, MONGOOSE_OPTIONS}
